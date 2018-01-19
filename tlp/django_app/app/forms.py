dc4ca527eda8b5b8fdd94b24583f11e0d0b18b1e
Munis Isazade <munisisazade@gmail.com>
2018-01-19 19:25:51 +0400

    fix oscar issues

---
+++ tlp/django_app/app/forms.py
@@ -0,0 +1,3 @@
+from django import form
+
+# Your forms here
---
+++ tlp/django_app/app/tasks.py
@@ -0,0 +1,5 @@
+# from __future__ import absolute_import, unicode_literals
+# from celery import shared_task
+
+# Use celery shared_task documentation here
+# http://docs.celeryproject.org/en/latest/faq.html
