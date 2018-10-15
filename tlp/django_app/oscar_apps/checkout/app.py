575a2c1e4b22d308ac3a9597b097f0fefd1f4c0e
Munis Isazade <munisisazade@gmail.com>
2018-10-15 16:04:28 +0400

    Fix bugs

--- tlp/django_app/oscar_apps/checkout/app.py
+++ tlp/django_app/oscar_apps/checkout/app.py
@@ -1,14 +1,15 @@
 from oscar.apps.checkout import app as checkout_app
-from .views import ShippingAddressView, UserAddressUpdateView, \
-    IndexView, PaymentDetailsView, ThankYouView
+# from .views import ShippingAddressView, UserAddressUpdateView, \
+#     IndexView, PaymentDetailsView, ThankYouView
 
 
 class CheckoutAppOscar(checkout_app.CheckoutApplication):
-    index_view = IndexView
-    shipping_address_view = ShippingAddressView
-    user_address_update_view = UserAddressUpdateView
-    payment_details_view = PaymentDetailsView
-    thankyou_view = ThankYouView
+    pass
+    # index_view = IndexView
+    # shipping_address_view = ShippingAddressView
+    # user_address_update_view = UserAddressUpdateView
+    # payment_details_view = PaymentDetailsView
+    # thankyou_view = ThankYouView
 
 
 application = CheckoutAppOscar()
